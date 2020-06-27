9cb443f4d6ad6052312b7bb9c29388de3d406e4d
Tomas Juarez <tomasjuarez@gmail.com>
2020-06-27 15:30:07 -0300

    1

--- api/store/create/index.php
+++ api/store/create/index.php
@@ -13,10 +13,9 @@ global $access_token, $collector_id, $integrator_id_test, $country_id;
  // Sustituye el método por su correspondiente: get, put, post, delete
  // Pista: Revisa detenidamente el endpoint y encuentra el fallo
 
-$url="";
+$url="https://api.mercadopago.com/users/$collector_id/stores?access_token=$access_token";
 $json = $_POST["json"];
 
-
-curl_call("put","https://api.mercadopago.com/users/$collector_id/store?access_token=$access_token",$json);
+curl_call("post",$url,$json);
 
 ?>
