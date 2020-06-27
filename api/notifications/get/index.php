5d4c802c0480ab4891c94f8cccee2c6adbe5c14b
Tomas Juarez <tomasjuarez@gmail.com>
2020-06-27 18:59:40 -0300

    Update index.php

--- api/notifications/get/index.php
+++ api/notifications/get/index.php
@@ -14,31 +14,6 @@ $lastResource = file_get_contents('../notifications.txt');
 $url= $lastResource."?access_token=".$access_token;
 
 
-
-
-
-$urlP = 'http://5.175.225.39:8080/ords/pelu/AgendaWeb/mp_ecommerce';
-	
-$curlP = curl_init($urlP);
-
-
-//creo un objeto JSON con los datos a enviar al servicio web
-$data = array(
-    'resultado' => $url
-);
-
-$payload = json_encode($data);
-
-//adjunto el objeto JSON al request
-curl_setopt($curlP, CURLOPT_POSTFIELDS, $payload);
-curl_setopt($curlP, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
-
-curl_setopt($curlP, CURLOPT_RETURNTRANSFER, true);
-$json_textoP = curl_exec($curlP);
-curl_close($curlP);
-
-
-
 // REVISAR AQUÍ:
 // Agrega la $url necesaria para revisar el estado del pago en base al recurso recibido de la notificación
 
